56f3368ea408d43c5569fd2362cbf689282c1efb
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-24 14:23:26 -0500

    Add initial GET /tasks endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
+import Task from './Task';
 
 const app = express();
 const port = 3000;
 
+const tasks: Task[] = [];
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
+});
+
+app.get('/tasks', (req: Request, res: Response) => {
+    res.json(tasks);
 });
 
 app.listen(port, () => {
