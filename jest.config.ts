fa1b46e6609b83ea768d32bd5158ab50ba309507
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-24 15:29:19 -0500

    Install and configure jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
+
+export default config
