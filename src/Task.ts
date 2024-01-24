aac8758a1c89cbcd56fe33f91abf91d61130c63e
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-24 15:20:40 -0500

    npm run lint:fix

--- src/Task.ts
+++ src/Task.ts
@@ -1,17 +1,17 @@
 interface Task {
-    id: string | number;
-    title: string;
-    description: string;
-    completed: boolean;
-  }
+  id: string | number
+  title: string
+  description: string
+  completed: boolean
+}
 
 export const equalTasks = (task1: Task, task2: Task): boolean => {
-    return (
-        task1.id === task2.id &&
-        task1.title === task2.title &&
-        task1.description === task2.description &&
-        task1.completed === task2.completed
-      );
-};
-  
-  export default Task;
+  return (
+    task1.id === task2.id &&
+    task1.title === task2.title &&
+    task1.description === task2.description &&
+    task1.completed === task2.completed
+  )
+}
+
+export default Task
--- src/app.ts
+++ src/app.ts
@@ -1,114 +1,114 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import Task, { equalTasks } from './Task';
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+import Task, { equalTasks } from './Task'
 
-const app = express();
-app.use(bodyParser.json());
-const port = 3000;
+const app = express()
+app.use(bodyParser.json())
+const port = 3000
 
-const tasks: Task[] = [];
+const tasks: Task[] = []
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+  res.send('Hello, World!')
+})
 
 app.get('/tasks', (req: Request, res: Response) => {
-    res.json(tasks);
-});
+  res.json(tasks)
+})
 
 const parseTaskId = (id: string): string | number => {
-    const numericId = parseInt(id, 10);
-    return isNaN(numericId) ? id : numericId;
+  const numericId = parseInt(id, 10)
+  return isNaN(numericId) ? id : numericId
 }
 
 app.get('/tasks/:id', (req: Request, res: Response) => {
-    const taskId =  parseTaskId(req.params.id);
+  const taskId = parseTaskId(req.params.id)
 
-    const task = tasks.find((t) => t.id === taskId);
+  const task = tasks.find((t) => t.id === taskId)
 
-    if (!task) {
-      res.status(404).json({ error: `Task ${req.params.id} not found` });
-    } else {
-      res.status(200).json(task);
-    }
-});
+  if (!task) {
+    res.status(404).json({ error: `Task ${req.params.id} not found` })
+  } else {
+    res.status(200).json(task)
+  }
+})
 
-let taskId = 1000;
+let taskId = 1000
 app.post('/tasks', (req: Request, res: Response) => {
-    const { id, title, description, completed }: Task = req.body;
-  
-    if (!title) {
-      res.status(400).json({ error: 'Invalid Task; title required' });
-      return;
-    }
-    if (id || id === 0) {
-        const existingTaskIndex = tasks.findIndex((t) => t.id === id);
+  const { id, title, description, completed }: Task = req.body
 
-        if (existingTaskIndex !== -1) {
-            res.status(409).json({ error: `Task ${taskId} already exists` });
-            return;
-        }
+  if (!title) {
+    res.status(400).json({ error: 'Invalid Task; title required' })
+    return
+  }
+  if (id || id === 0) {
+    const existingTaskIndex = tasks.findIndex((t) => t.id === id)
+
+    if (existingTaskIndex !== -1) {
+      res.status(409).json({ error: `Task ${taskId} already exists` })
+      return
     }
-  
-    const newTask: Task = {
-      id: id ?? taskId++,
-      title,
-      description: description ?? "",
-      completed: completed ?? false,
-    };
-  
-    tasks.push(newTask);
-  
-    res.status(201).json(newTask);
-});
+  }
+
+  const newTask: Task = {
+    id: id ?? taskId++,
+    title,
+    description: description ?? '',
+    completed: completed ?? false,
+  }
+
+  tasks.push(newTask)
+
+  res.status(201).json(newTask)
+})
 
 app.put('/tasks/:id', (req: Request, res: Response) => {
-    const taskId = parseTaskId(req.params.id);
+  const taskId = parseTaskId(req.params.id)
 
-    const existingTaskIndex = tasks.findIndex((t) => t.id === taskId);
+  const existingTaskIndex = tasks.findIndex((t) => t.id === taskId)
 
-    if (existingTaskIndex === -1) {
-        res.status(404).json({ error: `Task ${taskId} not found` });
-        return;
-    }
+  if (existingTaskIndex === -1) {
+    res.status(404).json({ error: `Task ${taskId} not found` })
+    return
+  }
 
-    const { id, title, description, completed }: Task = req.body;
+  const { id, title, description, completed }: Task = req.body
 
-    if (id && id !== taskId) {
-        res.status(405).json({ error: 'Unable to modify Task id' });
-        return;
-    }
+  if (id && id !== taskId) {
+    res.status(405).json({ error: 'Unable to modify Task id' })
+    return
+  }
 
-    const updatedTask: Task = {
-        id: taskId,
-        title,
-        description,
-        completed,
-    };
+  const updatedTask: Task = {
+    id: taskId,
+    title,
+    description,
+    completed,
+  }
 
-    if (equalTasks(tasks[existingTaskIndex], updatedTask)) {
-        res.status(304).json(updatedTask);
-        return;
-    }
+  if (equalTasks(tasks[existingTaskIndex], updatedTask)) {
+    res.status(304).json(updatedTask)
+    return
+  }
 
-    tasks[existingTaskIndex] = updatedTask;
+  tasks[existingTaskIndex] = updatedTask
 
-    res.json(updatedTask);
-});
+  res.json(updatedTask)
+})
 
 app.delete('/tasks/:id', (req: Request, res: Response) => {
-    const id = parseTaskId(req.params.id);
-  
-    const existingTaskIndex = tasks.findIndex((t) => t.id === id);
-  
-    if (existingTaskIndex === -1) {
-      res.status(404).json({ error: `Task ${id} not found` });
-    } else {
-      const deletedTask = tasks.splice(existingTaskIndex, 1)[0];
-      res.json(deletedTask);
-    }
-});
+  const id = parseTaskId(req.params.id)
+
+  const existingTaskIndex = tasks.findIndex((t) => t.id === id)
+
+  if (existingTaskIndex === -1) {
+    res.status(404).json({ error: `Task ${id} not found` })
+  } else {
+    const deletedTask = tasks.splice(existingTaskIndex, 1)[0]
+    res.json(deletedTask)
+  }
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
