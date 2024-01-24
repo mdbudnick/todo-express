ad1cebde851016d9441cee6aea5e59a9eb31229e
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-24 14:29:13 -0500

    Add initial GET /tasks/:id endpoint

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,19 @@ app.get('/tasks', (req: Request, res: Response) => {
     res.json(tasks);
 });
 
+app.get('/tasks/:id', (req: Request, res: Response) => {
+    const numericId = parseInt(req.params.id, 10);
+    const taskId = isNaN(numericId) ? req.params.id : numericId;
+
+    const task = tasks.find((t) => t.id === taskId);
+
+    if (!task) {
+      res.status(404).json({ error: `Task ${req.params.id} not found` });
+    } else {
+      res.status(200).json(task);
+    }
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
