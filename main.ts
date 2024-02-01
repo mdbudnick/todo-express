85818754d665ad15ecf9a1186f516130181a0d0e
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-31 19:31:27 -0500

    Start server with SSL certs

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 import app from './src/app'
+import https from 'https'
+import fs from 'fs'
+
+const privateKey = fs.readFileSync('privatekey.pem', 'utf8')
+const certificate = fs.readFileSync('server.crt', 'utf8')
+const credentials = { key: privateKey, cert: certificate }
 
 // eslint-disable-next-line no-undef
 const port = process.env.APP_PORT ?? 3001
-
-app.listen(port, () => {
+const httpsServer = https.createServer(credentials, app)
+httpsServer.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
